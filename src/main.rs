5edc4b0fb464df2105e9620c3ade94c8994bc89f
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2021-12-09 15:03:18 -0500

    Detect whether there are missing rows in the database and attempt to fill them.
    Without this change, a situation where the database has gaps (which
    for example could happen if epoch #123 is indexed successfully, but
    we get an error trying to fetch epoch #122) would have to be resolved
    manually.

--- src/main.rs
+++ src/main.rs
@@ -77,6 +77,7 @@ struct EpochInfo {
 
 #[derive(Debug)]
 struct EpochRef {
+    epoch_id: CryptoHash,
     epoch_height: EpochHeight,
     // hash of the last block in the previous epoch
     prev_last_block: CryptoHash,
@@ -85,6 +86,7 @@ struct EpochRef {
 impl EpochRef {
     fn from_info(epoch: &EpochInfo) -> Self {
         Self {
+            epoch_id: epoch.id,
             epoch_height: epoch.info.epoch_height,
             prev_last_block: epoch.first_block.header.prev_hash,
         }
@@ -245,9 +247,9 @@ impl EpochIndexer {
     async fn fetch_and_index_prev_epoch(
         &mut self,
         epoch_ref: &EpochRef,
-        last_indexed: EpochHeight,
+        until: EpochHeight,
     ) -> anyhow::Result<Option<EpochInfo>> {
-        if last_indexed >= epoch_ref.epoch_height - 1 {
+        if until >= epoch_ref.epoch_height - 1 {
             return Ok(None);
         }
         let epoch = match self.fetch_epoch(&epoch_ref.prev_last_block).await {
@@ -255,77 +257,118 @@ impl EpochIndexer {
             Ok(None) => return Ok(None),
             Err(e) => return Err(e),
         };
-        if last_indexed >= epoch.info.epoch_height {
+        if until >= epoch.info.epoch_height {
             return Ok(None);
+        }
+        if epoch.info.epoch_height == epoch_ref.epoch_height {
+            warn!(
+                "Found an epoch height repeat! Epochs {} and {} both have height #{}. Skipping...",
+                epoch.id, epoch_ref.epoch_id, epoch_ref.epoch_height
+            );
+            return Ok(Some(epoch));
+        }
+        if epoch.info.epoch_height > epoch_ref.epoch_height {
+            warn!(
+                "Epoch height sequence is descending from {} -> {}! (height #{} -> #{}). Skipping...",
+                epoch.id, epoch_ref.epoch_id, epoch.info.epoch_height, epoch_ref.epoch_height);
+            return Ok(Some(epoch));
+        }
+        if epoch.info.epoch_height != epoch_ref.epoch_height - 1 {
+            warn!(
+                "Found a gap in epoch heights! No epoch between #{} and #{} found.",
+                epoch.info.epoch_height, epoch_ref.epoch_height
+            );
         }
         self.save_validator_info(&epoch)?;
         Ok(Some(epoch))
     }
 
-    fn read_last_indexed_epoch(&self) -> anyhow::Result<EpochHeight> {
-        let last_indexed = schema::epochs::table
-            .select(schema::epochs::height)
-            .order_by(schema::epochs::height.desc())
-            .limit(1)
-            .load::<i32>(&self.db)?;
-        if last_indexed.len() == 0 {
-            return Ok(0);
-        }
-        if last_indexed.len() > 1 {
-            warn!("SELECT height FROM epochs ORDER BY height DESC LIMIT 1 unexpectedly returned more than 1 row...: {:?}", last_indexed);
-        }
-        Ok(EpochHeight::try_from(last_indexed[0]).unwrap_or_else(|_| {
-            error!(
-                "epochs table seems to contain a negative height!: {}",
-                last_indexed[0]
-            );
-            0
-        }))
-    }
-
-    async fn backfill(&mut self, mut epoch_ref: EpochRef) {
-        let last_indexed = match self.read_last_indexed_epoch() {
-            Ok(last) => last,
-            Err(e) => {
-                error!("Error reading last indexed epoch from the database: {}", e);
-                return;
-            }
+    fn read_indexed_epochs(
+        &self,
+        before: Option<EpochHeight>,
+        limit: usize,
+    ) -> anyhow::Result<Vec<(EpochHeight, BlockHeight)>> {
+        let query = schema::epochs::table
+            .select((schema::epochs::height, schema::epochs::start_height))
+            .order_by(schema::epochs::height.desc());
+        let rows = if let Some(before) = before {
+            query
+                .filter(schema::epochs::height.lt(i32::try_from(before).unwrap()))
+                .limit(limit.try_into().unwrap())
+                .load::<(i32, i64)>(&self.db)?
+        } else {
+            query
+                .limit(limit.try_into().unwrap())
+                .load::<(i32, i64)>(&self.db)?
         };
 
-        if last_indexed >= epoch_ref.epoch_height - 1 {
-            info!(
-                "The latest completed epoch #{} has already been saved. Nothing to do.",
-                last_indexed
-            );
-            return;
-        } else {
-            info!(
-                "The latest epoch is #{}, the last indexed was #{}",
-                epoch_ref.epoch_height, last_indexed
-            );
+        if rows.len() > limit {
+            return Err(anyhow!("SELECT height FROM epochs ORDER BY height DESC LIMIT {} unexpectedly returned {} rows...",
+			       limit, rows.len()));
         }
+        let mut ret = vec![];
 
-        let mut did_something = false;
+        for row in rows.iter() {
+            let height = match EpochHeight::try_from(row.0) {
+                Ok(h) => h,
+                Err(_) => {
+                    return Err(anyhow!(
+                        "epochs table seems to contain a negative height!: {}",
+                        row.0
+                    ));
+                }
+            };
+            let start_height = match BlockHeight::try_from(row.1) {
+                Ok(h) => h,
+                Err(_) => {
+                    return Err(anyhow!(
+                        "epochs table seems to contain a negative block height!: {}",
+                        row.1
+                    ));
+                }
+            };
+            ret.push((height, start_height));
+        }
+        Ok(ret)
+    }
+
+    fn read_num_epochs_indexed(&self) -> diesel::result::QueryResult<u64> {
+        Ok(schema::epochs::table
+            .count()
+            .get_result::<i64>(&self.db)?
+            .try_into()
+            .unwrap())
+    }
+
+    async fn backfill(
+        &mut self,
+        start: EpochHeight,
+        end: EpochHeight,
+        end_first_block: BlockHeight,
+    ) -> anyhow::Result<()> {
+        let mut epoch_ref = match self.fetch_block_at_height(end_first_block).await {
+            Ok(b) => EpochRef {
+                epoch_id: b.header.epoch_id,
+                epoch_height: end,
+                prev_last_block: b.header.prev_hash,
+            },
+            Err(e) => return Err(e),
+        };
 
         loop {
             let started_at = Instant::now();
             let start_req_count = self.req_count;
 
-            let epoch = match self
-                .fetch_and_index_prev_epoch(&epoch_ref, last_indexed)
+            let epoch = self
+                .fetch_and_index_prev_epoch(&epoch_ref, start)
                 .await
-            {
-                Ok(epoch) => epoch,
-                Err(e) => {
-                    // TODO: maybe try to keep going and save earlier ones
-                    error!("Error indexing validator info: {}", e);
-                    return;
-                }
-            };
+                .context(format!(
+                    "Failed to index epoch #{}",
+                    epoch_ref.epoch_height - 1
+                ))?;
 
             match epoch {
                 Some(epoch) => {
-                    did_something = true;
                     epoch_ref = EpochRef::from_info(&epoch);
                     std::thread::sleep(til_next_request(
                         self.options.backfill_qps,
@@ -334,10 +377,7 @@ impl EpochIndexer {
                     ));
                 }
                 None => {
-                    if !did_something {
-                        info!("The latest completed epoch has already been saved. Nothing to do.");
-                    }
-                    return;
+                    return Ok(());
                 }
             };
         }
@@ -396,6 +436,68 @@ impl EpochIndexer {
         }
     }
 
+    // returns true if there is more to go.
+    async fn index_missing_rows(
+        &mut self,
+        rows: &[(EpochHeight, BlockHeight)],
+        query_limit: usize,
+        smallest_indexed: &mut (EpochHeight, BlockHeight),
+        missing_rows: &mut u64,
+        latest: EpochHeight,
+        latest_start_height: BlockHeight,
+    ) -> bool {
+        for row in rows.iter() {
+            let height = row.0;
+
+            // the height == 1 check should not be needed since we check missing_rows,
+            // but keep it as a paranoid check
+            if height == 1 || *missing_rows == 0 {
+                return false;
+            }
+
+            if height >= smallest_indexed.0 {
+                error!(
+                    "epochs query with \"ORDER BY height DESC\" not strictly descending: {} -> {}",
+                    smallest_indexed.0, height
+                );
+                return false;
+            }
+            if height >= latest {
+                *missing_rows -= smallest_indexed.0 - height - 1;
+                *smallest_indexed = *row;
+                continue;
+            }
+            if height < smallest_indexed.0 - 1 && height < latest - 1 {
+                let result = if smallest_indexed.0 < latest {
+                    self.backfill(height, smallest_indexed.0, smallest_indexed.1)
+                        .await
+                } else {
+                    self.backfill(height, latest, latest_start_height).await
+                };
+                if let Err(e) = result {
+                    error!("{:#}", e);
+                    return false;
+                }
+                *missing_rows -= smallest_indexed.0 - height - 1;
+            }
+            *smallest_indexed = *row;
+        }
+        if rows.len() < query_limit {
+            let result = if smallest_indexed.0 < latest {
+                self.backfill(0, smallest_indexed.0, smallest_indexed.1)
+                    .await
+            } else {
+                self.backfill(0, latest, latest_start_height).await
+            };
+            if let Err(e) = result {
+                error!("{:#}", e);
+            }
+            false
+        } else {
+            true
+        }
+    }
+
     async fn run(&mut self) {
         if let Err(e) = self.check_chain_id().await {
             error!("Error checking chain ID: {}", e);
@@ -409,26 +511,119 @@ impl EpochIndexer {
                 return;
             }
         };
-
-        let first_block = match self
-            .fetch_block_at_height(latest_epoch.epoch_start_height)
-            .await
-        {
-            Ok(b) => b,
+        let num_rows = match self.read_num_epochs_indexed() {
+            Ok(n) => n,
             Err(e) => {
-                error!(
-                    "Error fetching block #{}: {}",
-                    latest_epoch.epoch_start_height, e
-                );
+                error!("Error reading num rows from the database: {}", e);
                 return;
             }
         };
+        let last_row = match self.read_indexed_epochs(None, 1) {
+            Ok(rows) => rows.first().copied(),
+            Err(e) => {
+                error!("Error querying the database: {:#}", e);
+                return;
+            }
+        };
+        let last_indexed = match last_row {
+            Some(row) => row.0,
+            None => 0,
+        };
+        let mut missing_rows = 0;
 
-        self.backfill(EpochRef {
-            epoch_height: latest_epoch.epoch_height,
-            prev_last_block: first_block.header.prev_hash,
-        })
-        .await;
+        if latest_epoch.epoch_height > last_indexed + 1 {
+            info!(
+                "There are new epochs to index. Current epoch = #{}. Last indexed = #{}.",
+                latest_epoch.epoch_height, last_indexed
+            );
+            missing_rows += latest_epoch.epoch_height - last_indexed - 1;
+        }
+        if last_indexed > num_rows {
+            info!(
+                "There seem to be some missing rows in the database. \
+		 The last indexed epoch has height #{} but there are only \
+		 {} rows. Attempting to backfill...",
+                last_indexed, num_rows
+            );
+            missing_rows += last_indexed - num_rows;
+        }
+        if missing_rows == 0 {
+            info!(
+                "The latest completed epoch #{} has already been saved. Nothing to do.",
+                last_indexed
+            );
+            return;
+        }
+
+        let mut smallest_indexed;
+
+        if let Some(row) = last_row {
+            let height = row.0;
+            if height < latest_epoch.epoch_height - 1 {
+                if let Err(e) = self
+                    .backfill(
+                        height,
+                        latest_epoch.epoch_height,
+                        latest_epoch.epoch_start_height,
+                    )
+                    .await
+                {
+                    error!("{:#}", e);
+                    return;
+                }
+
+                missing_rows -= latest_epoch.epoch_height - height - 1;
+                if missing_rows == 0 {
+                    return;
+                }
+            } else if height >= latest_epoch.epoch_height {
+                warn!(
+                    "{} gave latest epoch height = #{}. \
+		       Seems to be behind what has already been indexed (latest = #{})",
+                    self.options.rpc_url, latest_epoch.epoch_height, height
+                );
+            }
+            smallest_indexed = row;
+        } else {
+            if let Err(e) = self
+                .backfill(
+                    0,
+                    latest_epoch.epoch_height,
+                    latest_epoch.epoch_start_height,
+                )
+                .await
+            {
+                error!("{:#}", e);
+            }
+            return;
+        }
+
+        loop {
+            let limit = 100;
+            let heights = match self.read_indexed_epochs(Some(smallest_indexed.0), limit) {
+                Ok(heights) => heights,
+                Err(e) => {
+                    error!(
+                        "Error querying database for epochs earlier than #{}: {:#}",
+                        smallest_indexed.0, e
+                    );
+                    return;
+                }
+            };
+            if !self
+                .index_missing_rows(
+                    &heights,
+                    limit,
+                    &mut smallest_indexed,
+                    &mut missing_rows,
+                    latest_epoch.epoch_height,
+                    latest_epoch.epoch_start_height,
+                )
+                .await
+            {
+                return;
+            };
+        }
     }
 }
 
@@ -440,7 +635,6 @@ async fn main() {
         .init()
         .unwrap();
 
-    // TODO: add option to scan entire table and index any epochs not already in there.
     let matches = App::new("epoch-indexer")
         .arg(
             Arg::with_name("rpc-url")
@@ -470,6 +664,7 @@ async fn main() {
         rpc_url: matches.value_of("rpc-url").unwrap().to_string(),
         backfill_qps: clap::value_t!(matches, "backfill-max-qps", u32).unwrap(),
     };
+
     dotenv::dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let mut indexer = EpochIndexer::new(
